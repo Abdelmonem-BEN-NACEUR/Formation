9ebabdfcb8da8ac1a35b6f418647f7f5898342d5
abdelmonem <abdelmonem@abdelmonem-HP-Pavilion-15-Notebook-PC>
2018-08-25 19:16:06 +0100

    second one

--- Calculatrice/src/Calcul.java
+++ Calculatrice/src/Calcul.java
@@ -11,23 +11,24 @@ public class Calcul {
 		Air.add("l'air est nécesssaire à la vie de l'homme hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
 		Air.add("air is ok");
 		Air.add("air dans la plume");
-		
+
 		ArrayList<String> Eau = new ArrayList<String>();
 		Eau.add("l'eau potable");
 		Eau.add("eau dans les vaisseaux");
 		Eau.add("eau sanitaire de meme is ok");
-		
+
 		ArrayList<String> Terre = new ArrayList<String>();
 		Terre.add("la Terre est ronde");
 		Terre.add("Terre et lune");
 		Terre.add("sauvez la Terre avant qu'il soit trop tard!");
-		
-		HashMap<String, ArrayList<String>> Hmap= new HashMap<String,ArrayList<String>>();
+
+		HashMap<String, ArrayList<String>> Hmap = new HashMap<String, ArrayList<String>>();
 		Hmap.put("air", Air);
-		Hmap.put("eau",Eau);
-		Hmap.put("Terre",Terre);
-		
+		Hmap.put("eau", Eau);
+		Hmap.put("Terre", Terre);
+
 		System.out.println(Hmap.get("eau"));
-		
+
+		System.out.println(Hmap.get("eau"));
 	}
 }
