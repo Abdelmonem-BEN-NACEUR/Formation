2c8499869fe6e91a0e88623115964b6f27e7ec33
abdelmonem <abdelmonem@abdelmonem-HP-Pavilion-15-Notebook-PC>
2018-08-26 00:26:04 +0100

    6th one

--- Calculatrice/src/Files.java
+++ Calculatrice/src/Files.java
@@ -1,39 +1,46 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Scanner;
 
 public class Files {
-	static Scanner read=new Scanner(System.in);
-public static void main(String[] args) {
-	
-FileWriter f;
-try {
-	f = new FileWriter("test.txt");
-	f.write("Hello i'm abdelmonem");
-	f.close();
-} catch (IOException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
+	static Scanner read = new Scanner(System.in);
 
-try {
-	FileReader f2=new FileReader("test.txt");
-	int c;
-	while((c=f2.read())!=-1) {
-		System.out.print((char) c);
-		System.out.println("\n");
-		System.out.print(f2.read());
-		f2.close();
+	public static void main(String[] args) throws ClassNotFoundException {
+		
+		  Employee emp = new Employee(); emp.name = "hussain roubaye"; emp.age = 26;
+		  emp.dep = "software engeneer"; FileOutputStream fileout = null; try { fileout
+		  = new FileOutputStream("employee.txt");
+		  
+		  ObjectOutputStream out;
+		  
+		  out = new ObjectOutputStream(fileout); out.writeObject(emp); out.close();
+		  fileout.close();
+		  System.out.println("Serialized data is saved in ./employee.txt file"); }
+		  catch (IOException e) { // TODO Auto-generated catch block
+		  e.printStackTrace();
+		  
+		  }
+		 
+		Employee emp1 = null;
+		FileInputStream filein = null;
+		try {
+			filein = new FileInputStream("employee.txt");
+
+			ObjectInputStream in;
+
+			in = new ObjectInputStream(filein);
+			emp1 = (Employee) in.readObject();
+			in.close();
+			filein.close();
+			System.out.println(emp1.name + "\n" + emp1.age + " \n"+emp1.dep);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+		}
+
 	}
-} catch (FileNotFoundException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-} catch (IOException e) {
-	// TODO Auto-generated catch block
-	e.printStackTrace();
-}
-	
-}
 }
